151b9defbf04658904c6fff20d9ede4d24873bb1
Win <emertola@exist.com>
2021-06-06 00:48:20 +0800

    route exercise 1

--- app.js
+++ app.js
@@ -4,20 +4,19 @@ myAngularApp
   .config(function($routeProvider) {
     $routeProvider
       .when('/', {
-        template: '<h1>Home Page</h1>',
-        active: ''
+        // change to templateUrl
+        // point to the correct html file
+        template: ''
       })
       .when('/about', {
-        templateUrl: 'pages/about/index.html',
-        controller: 'AboutPageController',
-        active: 'about'
+        // change to templateUrl
+        template: ''
       })
       .when('/contact', {
-        template: '<h1>Contact Page</h1>',
-        active: 'contact'
+        // change to templateUrl
+        template: ''
       })
       .otherwise({
         template: '<h1>Page not found.</h1>'
-        // redirectTo: '/'
       });
   });
